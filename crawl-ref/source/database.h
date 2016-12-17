4c54eff31d52fa096c5dabf01cdd6ca344b033cb
dplusplus <sulpsulpd@gmail.com>
2016-12-18 01:02:53 +0900

    to_separated_fn(), to_separated_line()を追加

--- crawl-ref/source/database.h
+++ crawl-ref/source/database.h
@@ -61,4 +61,71 @@ string tagged_jtrans(const string &prefix, const string &key, const string &suff
 #define tagged_jtransc(p, k) (tagged_jtrans(p, k).c_str())
 string jtrans_notrim(const string &key);
 #define jtrans_notrimc(x) (jtrans_notrim(x).c_str())
+
+/**
+ * 渡されたシーケンスの各要素を変換しながら適切に繋ぐ
+ *
+ * [a, b]       -> "aとb"
+ * [a, b, c]    -> "aとb、そしてc"
+ * [a, b, c, d] -> "aとb、c、そしてd"
+ *
+ * stringifyにラムダ式を渡すことで各要素への前処理が可能
+ *
+ * @param start 処理を行うシーケンスの開始イテレータ
+ * @param end 処理を行うシーケンスの終端イテレータ
+ * @param stringify (string -> string)のラムダ式
+ * @param first 1番目と2番目の要素の間の区切り文字
+ * @param second n番目とn+1番目の要素の間の区切り文字(n > 2)
+ * @param fin last-1番目とlast番目の要素の間の区切り文字
+ *          　(first, secondの方を優先)
+ */
+template <typename Z, typename F>
+string to_separated_fn(Z start, Z end, F stringify,
+                       const string &first = "と",
+                       const string &second = "、",
+                       const string &fin = "、そして")
+{
+    string text;
+    for (Z i = start; i != end; ++i)
+    {
+        if (i != start)
+        {
+            if (prev(i) == start)
+                text += first;
+            else if (next(i) != end)
+                text += second;
+            else
+                text += fin;
+        }
+
+        text += stringify(*i);
+    }
+    return text;
+}
+
+/**
+ * 渡されたシーケンスの各要素を日本語化しながら適切に繋ぐ
+ * (to_separated_fn()の特殊版)
+ *
+ * [a, b]       -> "jtrans(a)とjtrans(b)"
+ * [a, b, c]    -> "jtrans(a)とjtrans(b)、そしてjtrans(c)"
+ * [a, b, c, d] -> "jtrans(a)とjtrans(b)、jtrans(c)、そしてjtrans(d)"
+ *
+ * @param start 処理を行うシーケンスの開始イテレータ
+ * @param end 処理を行うシーケンスの終端イテレータ
+ * @param first 1番目と2番目の要素の間の区切り文字
+ * @param second n番目とn+1番目の要素の間の区切り文字(n > 2)
+ * @param fin last-1番目とlast番目の要素の間の区切り文字
+ *          　(first, secondの方を優先)
+ */
+template<typename Z>
+string to_separated_line(Z start, Z end,
+                         const string &first = "と",
+                         const string &second = "、",
+                         const string &fin = "、そして")
+{
+    auto to_j = [] (const string &s) { return jtrans(s); };
+
+    return to_separated_fn(start, end, to_j, first, second, fin);
+}
 #endif
