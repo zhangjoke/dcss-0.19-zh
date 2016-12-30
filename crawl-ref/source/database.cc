9fd21b869b5a80886548b6716089e401190e4c5e
dplusplus <sulpsulpd@gmail.com>
2016-12-30 20:44:27 +0900

    jtrans_art_func.txtを0.16-jaからコピー

--- crawl-ref/source/database.cc
+++ crawl-ref/source/database.cc
@@ -149,8 +149,8 @@ static TextDB AllDBs[] =
            "jtrans_areas.txt",
            "jtrans_arena.txt",
            "jtrans_artefact.txt",
-           /*
            "jtrans_art_func.txt",
+           /*
            "jtrans_attack.txt",
            "jtrans_attitude_change.txt",
            "jtrans_beam.txt",
