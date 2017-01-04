8c5fd1c0299ea426f935182a50b0f7e2ea5325d7
dplusplus <sulpsulpd@gmail.com>
2017-01-05 00:55:14 +0900

    jtrans_attitude_change.txtを0.16-jaからコピー

--- crawl-ref/source/database.cc
+++ crawl-ref/source/database.cc
@@ -151,8 +151,8 @@ static TextDB AllDBs[] =
            "jtrans_artefact.txt",
            "jtrans_art_func.txt",
            "jtrans_attack.txt",
-           /*
            "jtrans_attitude_change.txt",
+           /*
            "jtrans_beam.txt",
            "jtrans_behold.txt",
            */
