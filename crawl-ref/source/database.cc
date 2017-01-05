4c6609c8f9f4fef11434f866df2196f088acc1c8
dplusplus <sulpsulpd@gmail.com>
2017-01-06 00:55:17 +0900

    jtrans_beam.txtを0.16-jaからコピー

--- crawl-ref/source/database.cc
+++ crawl-ref/source/database.cc
@@ -152,8 +152,8 @@ static TextDB AllDBs[] =
            "jtrans_art_func.txt",
            "jtrans_attack.txt",
            "jtrans_attitude_change.txt",
-           /*
            "jtrans_beam.txt",
+           /*
            "jtrans_behold.txt",
            */
            "jtrans_chardump.txt",
