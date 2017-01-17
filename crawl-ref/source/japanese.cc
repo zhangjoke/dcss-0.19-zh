26dcabbdb54069841987c6ddd385d5d692d0d5b2
dplusplus <sulpsulpd@gmail.com>
2017-01-18 00:37:28 +0900

    japanese.ccの不要なstatic配列を除去

--- crawl-ref/source/japanese.cc
+++ crawl-ref/source/japanese.cc
@@ -6,6 +6,7 @@
 
 #include "AppHdr.h"
 #include "database.h"
+#include "english.h"
 #include "japanese.h"
 #include "stringutil.h"
 
@@ -14,18 +15,8 @@ const string conjugate_verb_j(const string &verb, bool /* ignore */)
     return verb_j(verb);
 }
 
-static const char * const _pronoun_declension_j[][NUM_PRONOUN_CASES] =
-{
-    // subj  poss    refl        obj
-    { "it",  "its",  "itself",   "it"  }, // neuter
-    { "he",  "his",  "himself",  "him" }, // masculine
-    { "she", "her",  "herself",  "her" }, // feminine
-    { "you", "your", "yourself", "you" }, // 2nd person
-};
-
 const string decline_pronoun_j(gender_type gender, pronoun_type variant)
 {
-    COMPILE_CHECK(ARRAYSZ(_pronoun_declension_j) == NUM_GENDERS);
     ASSERT_RANGE(gender, 0, NUM_GENDERS);
     ASSERT_RANGE(variant, 0, NUM_PRONOUN_CASES);
 
@@ -43,16 +34,16 @@ const string decline_pronoun_j(gender_type gender, pronoun_type variant)
     {
     case GENDER_NEUTER:
         return tagged_jtrans(make_stringf("[gender neuter %s]", variant_tag),
-                             _pronoun_declension_j[gender][variant]);
+                             decline_pronoun(gender, variant));
     case GENDER_MALE:
         return tagged_jtrans(make_stringf("[gender male %s]", variant_tag),
-                             _pronoun_declension_j[gender][variant]);
+                             decline_pronoun(gender, variant));
     case GENDER_FEMALE:
         return tagged_jtrans(make_stringf("[gender female %s]", variant_tag),
-                             _pronoun_declension_j[gender][variant]);
+                             decline_pronoun(gender, variant));
     case GENDER_YOU:
         return tagged_jtrans(make_stringf("[gender you %s]", variant_tag),
-                             _pronoun_declension_j[gender][variant]);
+                             decline_pronoun(gender, variant));
     default:
         return make_stringf("buggy pronoun: %d, %d",
                             gender, variant);
