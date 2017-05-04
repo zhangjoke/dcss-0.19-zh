6f41faff5d29b8a5896ac1423658404fdf230a16
dplusplus <sulpsulpd@gmail.com>
2017-05-04 11:05:42 +0900

    jtrans_godconduct.txtを0.16-jaからコピー

--- crawl-ref/source/database.cc
+++ crawl-ref/source/database.cc
@@ -181,8 +181,8 @@ static TextDB AllDBs[] =
            "jtrans_godabil.txt",
            "jtrans_godblessing.txt",
            "jtrans_godcompanions.txt",
-           /*
            "jtrans_godconduct.txt",
+           /*
            "jtrans_godpassive.txt",
            "jtrans_godprayer.txt",
            "jtrans_godwrath.txt",
