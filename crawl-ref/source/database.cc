484fdd4a84797b243314c111669d311d4428ce39
dplusplus <sulpsulpd@gmail.com>
2017-09-24 17:40:08 +0900

    jtrans_mon_behv.txtとして0.16-jaからコピー

--- crawl-ref/source/database.cc
+++ crawl-ref/source/database.cc
@@ -205,6 +205,7 @@ static TextDB AllDBs[] =
            "jtrans_misc.txt",
            "jtrans_mon_abil.txt",
            "jtrans_mon_act.txt",
+           "jtrans_mon_behv.txt",
            /*
            "jtrans_autofight_lua.txt",
            "jtrans_magicspell.txt",
