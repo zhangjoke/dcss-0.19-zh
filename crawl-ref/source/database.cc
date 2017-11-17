9aa0b2bc21e6513353c5f9a236a6aa09cb717934
dplusplus <sulpsulpd@gmail.com>
2017-11-18 00:50:00 +0900

    jtrans_spl_monench.txtとして0.16-jaからコピー

--- crawl-ref/source/database.cc
+++ crawl-ref/source/database.cc
@@ -247,6 +247,7 @@ static TextDB AllDBs[] =
            "jtrans_spl_damage.txt",
            "jtrans_spl_goditem.txt",
            "jtrans_spl_miscast.txt",
+           "jtrans_spl_monench.txt",
 
            "jtrans_species.txt",
            "jtrans_travel.txt",
